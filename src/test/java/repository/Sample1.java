ac222373eb640f00d7cd1f44f3ebae71e6f08da8
Toshiba <Toshiba@DESKTOP-CSSJGEK>
2022-08-24 19:45:32 +0530

    coded

---
+++ src/test/java/repository/Sample1.java
@@ -0,0 +1,13 @@
+package repository;
+
+public class Sample1 {
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("sample repository");
+		System.out.println("git hub repository");
+		
+	}
+	
+}
---
+++ src/test/java/repository/Sample2.java
@@ -0,0 +1,13 @@
+package repository;
+
+public class Sample2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Github push command");
+		System.out.println("github commands");
+		
+		
+	}
+
+}
