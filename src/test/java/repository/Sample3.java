656f796cb05b3e637aaa7f8d21a277eb29ac1852
Toshiba <Toshiba@DESKTOP-CSSJGEK>
2022-08-27 17:48:10 +0530

    created code

---
+++ src/test/java/repository/Sample3.java
@@ -0,0 +1,13 @@
+package repository;
+
+public class Sample3 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("check with the github");
+		System.out.println("git hub repository");
+		
+		
+	}
+
+}
